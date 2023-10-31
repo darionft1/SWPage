4361f7079076337dcbb84029e3cbc6b6cefdc964
Darionft <85178125+Darionft1@users.noreply.github.com>
2023-10-31 17:16:31 -0300

    Final
    Final

--- script.js
+++ script.js
@@ -37,6 +37,42 @@ async function loadCharacters(url){
 
             characterNameBg.appendChild(characterName)
             card.appendChild(characterNameBg)
+
+            card.onclick = () =>{
+                const modal = document.getElementById('modal')
+                modal.style.visibility = 'visible'
+
+                const modalContent = document.getElementById('modalContent')
+                modalContent.innerHTML = ''
+
+                const characterImage = document.createElement('div')
+                characterImage.style.backgroundImage = `url("https://starwars-visualguide.com/assets/img/characters/${character.url.replace(/\D/g,'')}.jpg"`
+                characterImage.className = 'characterImage'
+                const characterName = document.createElement('span')
+                characterName.className = 'characterDetails'
+                characterName.innerHTML = `Name: ${character.name}`
+                const characterHeight = document.createElement('span')
+                characterHeight.className = 'characterDetails'
+                characterHeight.innerHTML = `Height: ${convertHeight(character.height)}`
+                const characterMass = document.createElement('span')
+                characterMass.className = 'characterDetails'
+                characterMass.innerHTML = `Mass: ${convertMass(character.mass)}`
+                const characterEyeColor = document.createElement('span')
+                characterEyeColor.className = 'characterDetails'
+                characterEyeColor.innerHTML = `Eye Color: ${convertEyeColor(character.eye_color)}`
+                const characterBirthYear = document.createElement('span')
+                characterBirthYear.className = 'characterDetails'
+                characterBirthYear.innerHTML = `Birth year: ${convertBirthYear(character.birth_year)}`
+                
+                modalContent.appendChild(characterImage);
+                modalContent.appendChild(characterName);
+                modalContent.appendChild(characterHeight);
+                modalContent.appendChild(characterMass);
+                modalContent.appendChild(characterEyeColor);
+                modalContent.appendChild(characterBirthYear);
+
+            }
+
             mainContent.appendChild(card)
 
         });
@@ -46,6 +82,7 @@ async function loadCharacters(url){
 
         nextButton.disabled = !responseJson.next
         backButton.disabled = !responseJson.previous
+
         backButton.style.visibility = responseJson.previous? 'visible' : 'hidden'
 
         currentPageURL = url;
@@ -54,4 +91,77 @@ async function loadCharacters(url){
         alert('Erro ao carregar os personagens.')
         console.log(error)
     }
+}
+
+async function loadNextPage() {
+    if(!currentPageURL) return;
+
+    try{
+        const response = await fetch(currentPageURL)
+        const responseJson = await response.json()
+
+        await loadCharacters(responseJson.next)
+    }catch (error){
+        console.log(error)
+        alert('erro ao carregar a proxima pagina.')
+    }
+}
+
+async function loadPreviousPage() {
+    if(!currentPageURL) return;
+
+    try{
+        const response = await fetch(currentPageURL)
+        const responseJson = await response.json()
+
+        await loadCharacters(responseJson.previous)
+    }catch (error){
+        console.log(error)
+        alert('erro ao carregar a pagina anterior.')
+    }
+}
+
+function hideModal (){
+    const modal = document.getElementById('modal')
+    modal.style.visibility = 'hidden'
+}
+
+function convertEyeColor(characterEyeColor) {
+    const cores = {
+        blue: 'azul',
+        brown: 'castanho',
+        green: 'verde',
+        yellow: 'amarelo',
+        black: 'preto',
+        pink: 'rosa',
+        red: 'vermelho',
+        orange: 'laranja',
+        hazel: 'avela',
+        unknown: 'desconhecido'
+    };
+    return cores [characterEyeColor.toLowerCase()] || characterEyeColor;
+    
+}
+
+function convertHeight(height){
+    if (height === 'Unknown'){
+        return 'desconhecida'
+    }
+    return (height /100).toFixed(2);
+}
+
+function convertMass(mass) {
+    if (mass === 'Unknown'){
+        return 'desconhecido'
+    }
+    
+    return `${mass} kg`
+}
+
+function convertBirthYear(birth_year){
+    if (birth_year === 'unknown'){
+        return 'desconhecido'
+    }
+
+    return birth_year
 }
